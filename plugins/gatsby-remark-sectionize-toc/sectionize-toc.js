3c2f4390715b2c44330437111833243941017495
Mateusz Filipowicz <matfilipowicz@gmail.com>
2020-07-03 16:03:03 +0200

    fix(toc): :bug: fix issue with sections creation when headers not start from h1

--- plugins/gatsby-remark-sectionize-toc/sectionize-toc.js
+++ plugins/gatsby-remark-sectionize-toc/sectionize-toc.js
@@ -5,25 +5,27 @@ const MAX_HEADING_DEPTH = 6;
 
 module.exports = () => transform;
 
-const transform = (tree, maxDepth) => {
+const transform = (tree, maxDepth, title) => {
   const maxTocDepth = maxDepth ? maxDepth : MAX_HEADING_DEPTH;
-  const visitFunction = sectionize(maxTocDepth);
+  const visitFunction = sectionize(maxTocDepth, title);
   for (let depth = MAX_HEADING_DEPTH; depth > 0; depth--) {
     visit(tree, (node) => node.type === 'heading' && node.depth === depth, visitFunction);
   }
 };
-const sectionize = (maxTocDepth) => {
+const sectionize = (maxTocDepth, title) => {
+  let minDepth = MAX_HEADING_DEPTH;
   return (node, ancestors) => {
     const start = node;
     const depth = start.depth;
     const parent = ancestors[ancestors.length - 1];
-
+    minDepth = depth < minDepth ? depth : minDepth;
     const isEnd = (node) =>
       (node.type === 'heading' && node.depth <= depth) ||
-      (node.type === 'section' && node.depth > depth && node.depth <= maxTocDepth) ||
+      // node.depth - (minDepth - 1) was added to fix case, when headers
+      // do not start from h1 or h2 (etc..)
+      (node.type === 'section' && node.depth > depth && (node.depth - (minDepth - 1) ) <= maxTocDepth) ||
       node.type === 'export';
     const end = findAfter(parent, start, isEnd);
-
     const startIndex = parent.children.indexOf(start);
     const endIndex = parent.children.indexOf(end);
 
